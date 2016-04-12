69803bb4234c8d61c24471a057feedace19627e9
Sean Johnson <sjohnson@sjohnsonaz.com>
2016-04-12 16:24:02 -0700

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var webpack = require('webpack-stream');
+
+gulp.task('js:build', function () {
+    return gulp.src('src/js/main.js')
+        .pipe(webpack(require('./webpack.config.js')))
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task("js:build-watch", ["js:build"], function () {
+    gulp.watch(["src/js/**/*"], ["js:build"]);
+});
+
+gulp.task('default', ['js:build']);
+
+gulp.task('dev', ['js:build-watch']);
---
+++ test/js/index.js
@@ -0,0 +1,61 @@
+$(function () {
+
+    $('#clickShowNotifications').click(function (event) {
+        $('.notification-container').toggleClass('hidden');
+    });
+
+    $('.tab-container').each(function (index, element) {
+        var ul = $($(element).children()[0]);
+        var div = $($(element).children()[1]);
+        ul.children().each(function (index, element) {
+            $(element).first('a').click(function (event) {
+                ul.children().removeClass('tab-active');
+                div.children().removeClass('tab-active');
+                $(element).addClass('tab-active');
+                $(div.children()[index]).addClass('tab-active');
+            });
+        });
+    });
+
+    var hidden = false;
+    $('#clickOpenModal, #clickCloseModal, .modal').click(function (event) {
+        event.stopPropagation();
+        hidden = !hidden;
+        //$('body').toggleClass('scroll-lock', hidden);
+        $('.modal').toggleClass('modal-open', hidden);
+    });
+    $('.modal-content').click(function (event) {
+        event.stopPropagation();
+    });
+
+    var carouselIndex = 0;
+    var carouselCount = 3;
+    var timeout;
+
+    function runCarousel(index) {
+        var carousel = $('.carousel');
+        var child = $(carousel.children()[index]);
+        carousel.height(carousel.outerHeight());
+        carousel.children().removeClass('carousel-selected');
+        carousel.children().removeClass('carousel-selecting');
+        child.addClass('carousel-selecting');
+        carousel.height(child.outerHeight());
+        window.clearTimeout(timeout);
+        timeout = window.setTimeout(function () {
+            child.removeClass('carousel-selecting');
+            child.addClass('carousel-selected');
+            carousel.height('auto');
+        }, 500);
+    }
+
+    $('#clickCarouselNext').click(function (event) {
+        carouselIndex++;
+        carouselIndex = (carouselIndex + carouselCount) % carouselCount;
+        runCarousel(carouselIndex);
+    });
+    $('#clickCarouselBack').click(function (event) {
+        carouselIndex--;
+        carouselIndex = (carouselIndex + carouselCount) % carouselCount;
+        runCarousel(carouselIndex);
+    });
+});
